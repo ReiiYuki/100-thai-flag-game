7cd9802120b7b208231770679e78884b70f839fd
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-11 22:06:49 +0700

    Apply some control in Flag Script

--- Assets/Scripts/FlagControl.cs
+++ Assets/Scripts/FlagControl.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlagControl : MonoBehaviour {
-
+    Rigidbody2D physicObject;
 	// Use this for initialization
 	void Start () {
-		
+        physicObject = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            physicObject.AddForce(new Vector2(-5f, 0f));
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            physicObject.AddForce(new Vector2(5f, 0f));
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            physicObject.AddForce(new Vector2(0f, 15f));
+    }
 }
