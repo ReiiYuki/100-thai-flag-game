7b7eb5873da83471b8d558c3e99c5556489f81cb
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-18 21:58:17 +0700

    Hover Action on Dummy Language Button

---
+++ Assets/Scripts/ButtonEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour {
+
+    public Sprite spriteNormal;
+    public Sprite spriteOnHover;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        GetComponent<SpriteRenderer>().sprite = spriteOnHover;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().sprite = spriteNormal;
+    }
+}
