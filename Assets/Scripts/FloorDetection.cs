a2cc59288f61cb9ee77a3ec97a01fac981e1b6c1
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-18 09:49:15 +0700

    Floor Detection Notification in Debug Log

---
+++ Assets/Scripts/FloorDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDetection : MonoBehaviour {
+    public GameObject flag;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject==flag)
+        {
+            Debug.Log("Lose");
+        }
+    }
+}
