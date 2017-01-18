ee1045771ea3ee8cc8e2d1f2494feea6db94cd67
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-18 18:06:37 +0700

    Change Scene is available

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+    private float totalTime;
+    private bool isDiscard;
+    public string scene;
+	// Use this for initialization
+	void Start () {
+        isDiscard = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        totalTime += Time.deltaTime;
+        if (totalTime > 1&&!isDiscard)
+        {
+            Application.LoadLevel(scene);
+            isDiscard = true;
+        }
+	}
+}
