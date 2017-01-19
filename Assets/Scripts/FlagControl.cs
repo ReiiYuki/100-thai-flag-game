14f88ba7173f6e8216f6ac223464ae6c3cec3193
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-19 13:49:55 +0700

    Wind Force But not sure is it correct T-T

--- Assets/Scripts/FlagControl.cs
+++ Assets/Scripts/FlagControl.cs
@@ -12,7 +12,7 @@ public class FlagControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-            physicObject.AddForce(new Vector2(-5f, 0f));
+            physicObject.AddForce(new Vector2(-15f, 0f));
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             physicObject.AddForce(new Vector2(5f, 0f));
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
