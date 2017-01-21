914df6a0f4c7b2534f4baaed9b719ddc35eea618
Pakpon Jetapai <pakpon.j@ku.th>
2017-01-21 17:49:56 +0700

    Obstacle class
    Add Obstacle script which is the base class of any future obstacles. Add
    Bird, HozinotalMovementBird script. Add Bird flying animation and its
    prefab.

---
+++ Assets/Scripts/HorizontalMovementBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A bird that moves horizontally
+public class HorizontalMovementBird : Bird {
+
+	public Vector2 originalPosition;
+	public Vector2 finalPosition;
+	public float startTime;
+	public float distance;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		originalPosition = gameObject.transform.position;
+		finalPosition = new Vector2(-gameObject.transform.position.x, gameObject.transform.position.y);
+		distance = Vector2.Distance (originalPosition, finalPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		this.Fly ();
+		float fracDistance = (Time.time - startTime) * flyingSpeed / distance;
+		gameObject.transform.position = Vector2.Lerp (originalPosition, finalPosition, fracDistance );
+	}
+
+	public override void Fly() {
+	}
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Base class for any future obstacles
+public class Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
