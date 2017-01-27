2628c38013aad666a95c4f1cd2e35c6d4cc6069f
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-27 21:27:44 +0700

    GetName

---
+++ Assets/Scripts/InputName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputName : MonoBehaviour {
+    public InputField nameField;
+	// Use this for initialization
+	void Start () {
+        nameField.onEndEdit.AddListener(delegate { SubmitName(); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void SubmitName()
+    {
+        Debug.Log(nameField.text);
+    }
+}
