1216542c4b63292b626ee189f4f44daca7cb26f1
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-28 01:12:06 +0700

    Get Token

--- Assets/Scripts/InputName.cs
+++ Assets/Scripts/InputName.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class InputName : MonoBehaviour {
     public InputField nameField;
-	// Use this for initialization
-	void Start () {
+    private const string SERVER_URL = "http://54.201.229.92:3000";
+    private const string GET_TOKEN = "/api/player/token/";
+    // Use this for initialization
+    void Start () {
         nameField.onEndEdit.AddListener(delegate { SubmitName(); });
+
 	}
 	
 	// Update is called once per frame
@@ -17,6 +20,21 @@ public class InputName : MonoBehaviour {
 
     void SubmitName()
     {
-        Debug.Log(nameField.text);
+        StartCoroutine(ConnectToServer());
+    }
+
+    IEnumerator ConnectToServer()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("player_name", nameField.text);
+        WWW request = new WWW(SERVER_URL + GET_TOKEN, form);
+        yield return request;
+        Token token = JsonUtility.FromJson<Token>(request.text);
+        Debug.Log(token.token);
+    }
+
+    class Token
+    {
+        public string token;
     }
 }
