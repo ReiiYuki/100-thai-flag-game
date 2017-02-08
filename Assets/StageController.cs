ec47ae4181779c9cb4b0dc2858df4c17c87c90e0
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-08 10:10:13 +0700

    BG Spawn Condition

--- Assets/StageController.cs
+++ Assets/StageController.cs
@@ -40,175 +40,175 @@ public class StageController : MonoBehaviour {
         if (!stageBG[0]&&GetCurrentHeight()>=0)
         {
 
-        }else if (!stageBG[1] && GetCurrentHeight() >= 0)
+        }else if (!stageBG[1] && GetCurrentHeight() >= 45)
         {
 
         }
-        else if (!stageBG[2] && GetCurrentHeight() >= 0)
+        else if (!stageBG[2] && GetCurrentHeight() >= 61)
         {
 
         }
-        else if (!stageBG[3] && GetCurrentHeight() >= 0)
+        else if (!stageBG[3] && GetCurrentHeight() >= 80)
         {
 
         }
-        else if (!stageBG[4] && GetCurrentHeight() >= 0)
+        else if (!stageBG[4] && GetCurrentHeight() >= 112)
         {
 
         }
-        else if (!stageBG[5] && GetCurrentHeight() >= 0)
+        else if (!stageBG[5] && GetCurrentHeight() >= 145)
         {
 
         }
-        else if (!stageBG[6] && GetCurrentHeight() >= 0)
+        else if (!stageBG[6] && GetCurrentHeight() >= 170)
         {
 
         }
-        else if (!stageBG[7] && GetCurrentHeight() >= 0)
+        else if (!stageBG[7] && GetCurrentHeight() >= 188)
+        {   
+
+        }
+        else if (!stageBG[8] && GetCurrentHeight() >= 255)
         {
 
         }
-        else if (!stageBG[8] && GetCurrentHeight() >= 0)
+        else if (!stageBG[9] && GetCurrentHeight() >= 394)
         {
 
         }
-        else if (!stageBG[9] && GetCurrentHeight() >= 0)
+        else if (!stageBG[10] && GetCurrentHeight() >= 310)
         {
 
         }
-        else if (!stageBG[10] && GetCurrentHeight() >= 0)
+        else if (!stageBG[11] && GetCurrentHeight() >= 361)
         {
 
         }
-        else if (!stageBG[11] && GetCurrentHeight() >= 0)
+        else if (!stageBG[12] && GetCurrentHeight() >= 401)
         {
 
         }
-        else if (!stageBG[12] && GetCurrentHeight() >= 0)
+        else if (!stageBG[13] && GetCurrentHeight() >= 489)
         {
 
         }
-        else if (!stageBG[13] && GetCurrentHeight() >= 0)
+        else if (!stageBG[14] && GetCurrentHeight() >= 521)
         {
 
         }
-        else if (!stageBG[14] && GetCurrentHeight() >= 0)
+        else if (!stageBG[15] && GetCurrentHeight() >= 580)
         {
 
         }
-        else if (!stageBG[15] && GetCurrentHeight() >= 0)
+        else if (!stageBG[16] && GetCurrentHeight() >= 614)
         {
 
         }
-        else if (!stageBG[16] && GetCurrentHeight() >= 0)
+        else if (!stageBG[17] && GetCurrentHeight() >= 809)
         {
 
         }
-        else if (!stageBG[17] && GetCurrentHeight() >= 0)
+        else if (!stageBG[18] && GetCurrentHeight() >= 980)
         {
 
         }
-        else if (!stageBG[18] && GetCurrentHeight() >= 0)
+        else if (!stageBG[19] && GetCurrentHeight() >= 1930)
         {
 
         }
-        else if (!stageBG[19] && GetCurrentHeight() >= 0)
+        else if (!stageBG[20] && GetCurrentHeight() >= 1980)
         {
 
         }
-        else if (!stageBG[20] && GetCurrentHeight() >= 0)
+        else if (!stageBG[21] && GetCurrentHeight() >= 2580)
         {
 
         }
-        else if (!stageBG[21] && GetCurrentHeight() >= 0)
+        else if (!stageBG[22] && GetCurrentHeight() >= 2980)
         {
 
         }
-        else if (!stageBG[22] && GetCurrentHeight() >= 0)
+        else if (!stageBG[23] && GetCurrentHeight() >= 3756)
         {
 
         }
-        else if (!stageBG[23] && GetCurrentHeight() >= 0)
+        else if (!stageBG[24] && GetCurrentHeight() >= 4980)
         {
 
         }
-        else if (!stageBG[24] && GetCurrentHeight() >= 0)
+        else if (!stageBG[25] && GetCurrentHeight() >= 4105)
         {
 
         }
-        else if (!stageBG[25] && GetCurrentHeight() >= 0)
+        else if (!stageBG[26] && GetCurrentHeight() >= 4401)
         {
 
         }
-        else if (!stageBG[26] && GetCurrentHeight() >= 0)
+        else if (!stageBG[27] && GetCurrentHeight() >= 4787)
         {
 
         }
-        else if (!stageBG[27] && GetCurrentHeight() >= 0)
+        else if (!stageBG[28] && GetCurrentHeight() >= 5469)
         {
 
         }
-        else if (!stageBG[28] && GetCurrentHeight() >= 0)
+        else if (!stageBG[29] && GetCurrentHeight() >= 6170)
         {
 
         }
-        else if (!stageBG[29] && GetCurrentHeight() >= 0)
+        else if (!stageBG[30] && GetCurrentHeight() >= 6549)
         {
 
         }
-        else if (!stageBG[30] && GetCurrentHeight() >= 0)
+        else if (!stageBG[31] && GetCurrentHeight() >= 6980)
         {
 
         }
-        else if (!stageBG[31] && GetCurrentHeight() >= 0)
+        else if (!stageBG[32] && GetCurrentHeight() >= 7634)
         {
 
         }
-        else if (!stageBG[32] && GetCurrentHeight() >= 0)
+        else if (!stageBG[33] && GetCurrentHeight() >= 7980)
         {
 
         }
-        else if (!stageBG[33] && GetCurrentHeight() >= 0)
+        else if (!stageBG[34] && GetCurrentHeight() >= 8828)
         {
 
         }
-        else if (!stageBG[34] && GetCurrentHeight() >= 0)
+        else if (!stageBG[35] && GetCurrentHeight() >= 11867)
         {
 
         }
-        else if (!stageBG[35] && GetCurrentHeight() >= 0)
+        else if (!stageBG[36] && GetCurrentHeight() >= 15380)
         {
 
         }
-        else if (!stageBG[36] && GetCurrentHeight() >= 0)
+        else if (!stageBG[37] && GetCurrentHeight() >= 17980)
         {
 
         }
-        else if (!stageBG[37] && GetCurrentHeight() >= 0)
+        else if (!stageBG[38] && GetCurrentHeight() >= 20059)
         {
 
         }
-        else if (!stageBG[38] && GetCurrentHeight() >= 0)
+        else if (!stageBG[39] && GetCurrentHeight() >= 23980)
         {
 
         }
-        else if (!stageBG[39] && GetCurrentHeight() >= 0)
+        else if (!stageBG[40] && GetCurrentHeight() >= 111980)
         {
 
         }
-        else if (!stageBG[40] && GetCurrentHeight() >= 0)
+        else if (!stageBG[41] && GetCurrentHeight() >= 399980)
         {
 
         }
-        else if (!stageBG[41] && GetCurrentHeight() >= 0)
+        else if (!stageBG[42] && GetCurrentHeight() >= 588980)
         {
 
         }
-        else if (!stageBG[42] && GetCurrentHeight() >= 0)
-        {
-
-        }
-        else if (!stageBG[43] && GetCurrentHeight() >= 0)
+        else if (!stageBG[43] && GetCurrentHeight() >= 3299980)
         {
 
         }
