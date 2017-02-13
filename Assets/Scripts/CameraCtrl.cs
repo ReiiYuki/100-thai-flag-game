2883a5e0c07407f0400577f96cf3dc4e8383ae5e
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-13 19:18:08 +0700

    BG

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+
+    // The gameObject that this camera will follow all the way.
+    private GameObject playerFlag;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get player flag by finding FlagCtrl component which is attached to playerFlag
+        playerFlag = FindObjectOfType<FlagCtrl>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change main camera transform's position
+        //Only y-axis value changes
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x,
+                                                        playerFlag.transform.position.y,
+                                                        Camera.main.transform.position.z);
+    }
+}
