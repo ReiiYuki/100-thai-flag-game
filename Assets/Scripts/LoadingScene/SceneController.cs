fd6ea624636f4879bb7afcdccd98a29491876efe
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-14 21:48:19 +0700

    Scene Transaction

--- Assets/Scripts/LoadingScene/SceneController.cs
+++ Assets/Scripts/LoadingScene/SceneController.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
-    public string nextScene;
 
     void Start () {
         StartCoroutine(SwapScene());
@@ -12,6 +12,6 @@ public class SceneController : MonoBehaviour {
     IEnumerator SwapScene()
     {
         yield return new WaitForSeconds(2);
-        Debug.Log("OK");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
