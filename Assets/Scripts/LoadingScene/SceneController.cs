8c216360ec303d4932b1048847fb0ed25744e1c6
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-14 21:38:51 +0700

    Fading

---
+++ Assets/Scripts/LoadingScene/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+    public string nextScene;
+
+    void Start () {
+        StartCoroutine(SwapScene());
+	}
+
+    IEnumerator SwapScene()
+    {
+        yield return new WaitForSeconds(2);
+        Debug.Log("OK");
+    }
+}
