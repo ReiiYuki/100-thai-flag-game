72032df2360e63ef66e4f3fe28595020f5a21448
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-15 09:32:12 +0700

    Multi language in Scoreboard

---
+++ Assets/Scripts/Scoreboard/ScoreHeaderLang.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHeaderLang : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SetLanguage();
+	}
+	
+	void SetLanguage()
+    {
+        string lang = PlayerPrefs.GetString("lang");
+        if (lang == "th")
+            GetComponent<Text>().text = "ตารางคะแนน";
+    }
+}
