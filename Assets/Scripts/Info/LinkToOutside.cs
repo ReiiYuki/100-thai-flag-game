902278092a1c3b72ef83f1c895058bf11d020e38
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-15 19:13:33 +0700

    Open Outside Link

--- Assets/Scripts/Info/LinkToOutside.cs
+++ Assets/Scripts/Info/LinkToOutside.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class LinkToOutside : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string url;
+
+    void OnMouseDown()
+    {
+        Application.OpenURL(url);
+    }
 }
