a1c0207abdc6308041024d47df192d34f7465c1b
Pakpon Jetapai <pakpon.j@ku.th>
2017-02-16 21:16:04 +0700

    SinosoidalMovementBird script now move back and forth

--- Assets/Scripts/SinusoidalMovementBird.cs
+++ Assets/Scripts/SinusoidalMovementBird.cs
@@ -4,17 +4,55 @@ using UnityEngine;
 
 public class SinusoidalMovementBird : Bird {
 
+	// Equation: y = sin(x)
+
+	// X each frame = Current position + ( flying speed x time between frame )
 	public float position_x;
+
+	// Y each frame = Sin ( (PI x 2) / 360 x X x 90 )
 	public float position_y;
+
+	// Max distance the bird travels from the center
+	public float maxDistance;
+
+	private Vector2 nextPosition;
+
+	public int direction;
+	public bool hasFlipped;
+
+	void Start() {
+		maxDistance = 4.5f;
+		nextPosition = Vector2.zero;
+		if (gameObject.transform.position.x >= 0) {
+			direction = 1;
+		} else {
+			direction = -1;
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
 		Fly ();
 	}
 
+	// Override method from super class
 	public override void Fly() {
-		position_x = gameObject.transform.position.x + (flyingSpeed * Time.deltaTime);
+		if (canFlip ()) {
+			direction *= -1;
+		} 
+		position_x = gameObject.transform.position.x + (flyingSpeed * Time.deltaTime * direction);
 		position_y = Mathf.Sin ( Mathf.Deg2Rad * position_x * 90 );
-		gameObject.transform.position = new Vector2 (position_x, position_y);
+		setNextPosition (position_x, position_y);
 	}
+
+	public bool canFlip() {
+		return Mathf.Abs(gameObject.transform.position.x) >= maxDistance;
+	}
+
+	private void setNextPosition(float x, float y) {
+		nextPosition.x = x;
+		nextPosition.y = y;
+		gameObject.transform.position = nextPosition;
+	}
+
 }
