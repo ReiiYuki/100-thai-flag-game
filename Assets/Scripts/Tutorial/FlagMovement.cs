dbafcd10df1ea89e7a452ea5d8c7036003604d03
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-17 00:27:34 +0700

    Tutorial Page

---
+++ Assets/Scripts/Tutorial/FlagMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagMovement : MonoBehaviour
+{
+
+    public string state;
+    Vector2 origin;
+
+    // Use this for initialization
+    void Start()
+    {
+        state = "l";
+        origin = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == "l")
+            transform.Translate(new Vector2(-1f, 2f) * 1f * Time.deltaTime);
+        else if (state == "c")
+            transform.Translate(new Vector2(0, 1f) * 2f * Time.deltaTime);
+        else if (state == "r")
+            transform.Translate(new Vector2(1f, 2f) * 1f * Time.deltaTime);
+
+        if (state == "l" && transform.position.x < -2)
+        {
+            transform.position = origin;
+            state = "r";
+        }
+        if (state == "c" && transform.position.y > 3)
+        {
+            transform.position = origin;
+            state = "l";
+        }
+        if (state == "r" && transform.position.x > 2)
+        {
+            transform.position = origin;
+            state = "c";
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Tutorial/MobilePlatformTutorial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilePlatformTutorial : MonoBehaviour {
+
+    public GameObject L, R;
+    FlagMovement flag;
+
+    // Use this for initialization
+    void Start()
+    {
+        flag = GameObject.FindGameObjectWithTag("Player").GetComponent<FlagMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag.state == "l")
+        {
+            L.SetActive(true);
+            R.SetActive(false);
+        }
+        else if (flag.state == "c")
+        {
+            L.SetActive(true);
+            R.SetActive(true);
+        }
+        else if (flag.state == "r")
+        {
+            L.SetActive(false);
+            R.SetActive(true);
+        }
+
+    }
+}
